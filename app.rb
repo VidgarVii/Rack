b73c823deb735ec1e2d2283ab9bee0fe7a128c4f
Pavel <vidasp@mail.ru>
2019-02-05 09:49:40 +0300

    refact app & Date

--- app.rb
+++ app.rb
@@ -6,22 +6,20 @@ class App
 
   def call(env)
     @request = Rack::Request.new(env)
-    return response_time if request_time?
+    return make_response(404, 'Page not found') unless request_time?
 
-    make_response(404, 'Page not found')
+    format_date = DateService.new(@request.params['format'])
+    if format_date.valid?
+      make_response(200, format_date.formatted_time)
+    else
+      make_response(400, format_date.invalid_formats)
+    end
   end
 
   private
 
   def request_time?
     @request.get? && @request.path == '/time' && @request.params['format']
-  end
-
-  def response_time
-    format_date = DateService.new(@request.params['format'])
-    return make_response(200, format_date.formatted_time) if format_date.valid?
-
-    make_response(400, format_date.invalid_formats)
   end
 
   def make_response(status, body)
